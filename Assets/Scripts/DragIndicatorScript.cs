68d9bd0b71d67b21c5475fc0badff4996e56b9e0
lando10 <orlando.avila@tortoise.dev>
2021-01-06 19:08:01 -0600

    Drag and pull project.

---
+++ Assets/Scripts/DragIndicatorScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragIndicatorScript : MonoBehaviour
+{
+    Vector3 startPos;
+    Vector3 endPos;
+    Camera camera;
+    LineRenderer lr;
+
+    void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            
+        }
+    }
+}
